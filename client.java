b4be314dd569416e95123b5717b3e82fe91a7e17
mmakos <37179289+mmakos@users.noreply.github.com>
2020-04-24 23:44:12 +0200

    Wysyłanie i odbieranie powinno działać z serwerem c++

--- client.java
+++ client.java
@@ -2,32 +2,34 @@ import java.net.*;
 import java.io.*;
 import java.util.Scanner;
 
-public class client{
+public class Client{
 	public static void main( String args[] ) throws IOException{
-		Socket s = new Socket( "25.139.176.21", 54111 );
-		DataInputStream din = new DataInputStream( s.getInputStream() );
+		Socket s = new Socket( "185.20.175.81", 54000 );
+		BufferedReader din = new BufferedReader( new InputStreamReader( s.getInputStream() ) );
 		PrintWriter dout = new PrintWriter( s.getOutputStream(), true );
 		Scanner scan = new Scanner( System.in );
-		
+
 		Thread writer = new Thread( () -> {
+			//try{
 				String messageOut = "";
 				while( !messageOut.equals( "q" ) && !messageOut.equals( "quit" ) ){
 					dout.println( messageOut );
 					messageOut = scan.nextLine();
 				}
 				return;
+			//} catch( IOException ex ){};
 		});
 		Thread reader = new Thread( () -> {
-			try{	
+			try{
 				String messageIn = "";
 				while( true ){
-					messageIn = din.readUTF();
+					messageIn = din.readLine();
 					System.out.print( "Server: " + messageIn + "\n" );
 					messageIn = "";
 				}
 			} catch( IOException ex ){};
 		});
-		
+
 		writer.start();
 		reader.start();
 	}
