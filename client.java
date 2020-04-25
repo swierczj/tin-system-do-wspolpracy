7353eba1639270e69a08552ec8a9284a53162288
mmakos <37179289+mmakos@users.noreply.github.com>
2020-04-25 20:10:12 +0200

    Są parametry wywołania i obsługa wyjątków

--- client.java
+++ client.java
@@ -4,7 +4,22 @@ import java.util.Scanner;
 
 public class Client{
 	public static void main( String args[] ) throws IOException{
-		Socket s = new Socket( "localhost", 54000 );
+		Socket s;
+		String ip = "localhost";
+		int port = 54000;
+		
+		if( args.length > 0 )
+			ip = args[ 0 ];
+		if( args.length > 1 )
+			port = Integer.parseInt( args[ 1 ] );
+		
+		try{
+			s = new Socket( ip, port );
+		}catch( ConnectException cEx ){ 
+			System.out.print( "Cannot connect to " + ip + " on port " + port );
+			return; 
+		};
+		
 		BufferedReader din = new BufferedReader( new InputStreamReader( s.getInputStream() ) );
 		PrintWriter dout = new PrintWriter( s.getOutputStream(), true );
 		Scanner scan = new Scanner( System.in );
