ab29d4259c8ad782ce869bbecc9deaf5525c0d14
swierczj <kub9818@gmail.com>
2020-04-26 23:27:58 +0200

    fix needed v2

--- server/main.cpp
+++ server/main.cpp
@@ -5,21 +5,23 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <string>
-#include <string.h>
+#include <cstring>
 #include <sys/ioctl.h>
 #include "serv_func.h"
 
 //windows
-/*
 #include <winsock.h>
 #include <windows.h>
 #include <ws2tcpip.h>
-*/
+
 
 int main()
 {
     auto port_num = htons(54000);
     bool run_server = true;
+    bool close_conn = false;
+    int buff_size = 4096;
+    char buffer[buff_size];
     int listening = socket(AF_INET, SOCK_STREAM, 0);
     if (listening == -1)
     {
@@ -96,15 +98,15 @@ int main()
 
         // counting ready descriptors
         int desc_ready = socket_count;
-        for (int i = 0; i <= max_sd && desc_ready > 0; i++)
+        for (int descriptor = 0; descriptor <= max_sd && desc_ready > 0; descriptor++)
         {
-            // auto tmp_socket = working_set.fd_array[i]; // for windows impl, unix has not got fd_array
+            // auto tmp_socket = working_set.fd_array[descriptor]; // for windows impl, unix has not got fd_array
             // check if this descriptor is ready
-            if (FD_ISSET(i, &working_set))
+            if (FD_ISSET(descriptor, &working_set))
             {
                 desc_ready -= 1; // in order to stop loop when all ready descriptors are found
                 // check if descriptor refers to listening socket
-                if (i == listening)
+                if (descriptor == listening)
                 {
                     std::cout << "listening socket is readable" << std::endl;
                     // accept all incoming connection, if accept() fails with EWOULDBLOCK
@@ -117,7 +119,7 @@ int main()
                         socklen_t client_size = sizeof(client);
                         new_cli = accept(listening, (sockaddr *)&client, &client_size);
                         //new_cli = accept(listening, nullptr, nullptr);
-                        if (new_cli == -1)
+                        if (new_cli < 0)
                         {
                             if (errno != EWOULDBLOCK)
                             {
@@ -131,23 +133,76 @@ int main()
                         std::cout << "new incoming connection descriptor: " << new_cli << std::endl;
                         // add incoming connection to master set
                         FD_SET(new_cli, &master);
+                        std::string welcome_msg = "Hello from server!\n";
+                        send(new_cli, welcome_msg.c_str(), welcome_msg.size() + 1, 0);
                         print_host(client);
                         if (new_cli > max_sd)
                             max_sd = new_cli;
                         // loop back and accept another incoming connection
                     } // while new_cli != -1
-                } // if i == listening
+                } // if descriptor == listening
 
                 // not a listening socket, but readable
-            } // if ISSET
-            // else
-            // {
-            //     // accept new mess
-            // }
-        }
+                else
+                {
+                    std::cout << "descriptor " << descriptor << " is readable" << std::endl;
+                    close_conn = false;
+                    // receive all incoming data on this socket before looping back and calling select()
+                    while (true)
+                    {
+                        memset(buffer, 0, buff_size);
+                        // check if recv blocks
+                        int bytes_recv = recv(descriptor, buffer, buff_size, 0);
+                        if (bytes_recv < 0)
+                        {
+                            if (errno != EWOULDBLOCK)
+                            {
+                                std::cerr << "recv() error" << std::endl;
+                                close_conn = true;
+                            }
+                            break;
+                        }
+                        // check if the connection closed by client
+                        if (bytes_recv == 0)
+                        {
+                            std::cout << "client disconnected" << std::endl;
+                            close_conn = true;
+                            break;
+                        }
+
+                        //echo message back
+                        std::cout << "received: " << std::string(buffer, 0, bytes_recv);
+                        std::string echo_msg = "echo from server: ";
+                        echo_msg.append(std::string(buffer, 0, bytes_recv));
+                        if (send(descriptor, echo_msg.c_str(), echo_msg.size() + 1, 0) < 0)
+                        {
+                            std::cerr << "send() error" << std::endl;
+                            close_conn = true;
+                            break;
+                        }
+
+                        // send other clients the message
+                        // for (int out_socket = 0; out_socket < master.fd_count; out_socket++)
+
+                    } // while
+                    // if the close_conn was set true, we need to clean up and change number of descriptors
+                    if (close_conn)
+                    {
+                        close(descriptor);
+                        FD_CLR(descriptor, &master);
+                        if (descriptor == max_sd)
+                        {
+                            // look for new max_sd value
+                            while (FD_ISSET(max_sd, &master) == false)
+                                max_sd -= 1; // decrement max_sd value till found new max descriptor value
+                        }
+                    }
+                } // end of readable descriptor
+            } // end of if ISSET
+        } // end of loop through selectable descriptors
     }
     // cleanup of open sockets
-    for (int socket = 0; i <= max_sd; i++)
+    for (int socket = 0; socket <= max_sd; socket++)
     {
         if (FD_ISSET(socket, &master))
             close(socket);
