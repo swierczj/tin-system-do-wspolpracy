4b81d5ee054db2001c34b4e12d3bdb7b6e37de06
Bartłomiej Wolski <rudy10003@gmail.com>
2020-04-28 22:01:28 +0200

    client exiting on lost connection

--- client_test.py
+++ client_test.py
@@ -4,9 +4,20 @@ import selectors
 import threading
 import sys
 import time
+import logging as log
 
+
+log.basicConfig(stream=sys.stdout, level=log.DEBUG,
+                format='%(asctime)s - %(levelname)s - %(message)s')
+#log.disable(log.CRITICAL)
+
+HOST1 = "25.139.176.21"
 HOST = "127.0.0.1"
-PORT = 55555
+PORT = 54000
+
+CONNECTION_ATTEMPTS = 2
+
+MSG_LEN = 64
 
 
 class Client(threading.Thread):
@@ -19,7 +30,7 @@ class Client(threading.Thread):
 
     def run(self) -> None:
         self.create_socket()
-        self.connect()
+        self.connect(CONNECTION_ATTEMPTS)
         self.send_receive_loop()
 
 
@@ -27,61 +38,102 @@ class Client(threading.Thread):
         try:
             self.client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         except socket.error:
-            print("Unable to create socket.\n Terminating program ")
+            log.debug("Unable to create socket. Terminating program" )
             sys.exit()
 
 
-    def connect(self):
-        try:
-            self.client_socket.connect((self.host,self.port))
-            self.client_socket.setblocking(0)
-            print(f"Connection to  :succesful")
-        except socket.error as e:
-            print(f"Unable to connect:\n {e}\n")
-            self.client_socket.close()
-            sys.exit()
+    def connect(self,no_attempts):
+        for it in range(no_attempts):
+            try:
+                self.client_socket.connect((self.host,self.port))
+                self.client_socket.setblocking(0)
+
+                log.debug(f"Connection to {self.host}:{self.port} succesful")
+                break
+
+            except socket.error as e:
+                log.debug(f"Attempt:[{it}] Connection to {self.host}:{self.port} failed.")
+                log.debug(f"{e}")
+
+                if(it < no_attempts-1):
+                    continue
+                self.client_socket.close()
+                sys.exit()
 
 
     def send_receive_loop(self):
         sl = [self.client_socket] #  sl as in socket list but there is only one socket
-        i = 0
+
+        i = 0 #dummy message generator
         k = 0
+
         while True:
+            i+=1 #dummy iterator
             read_socket,write_socket,exception_socket = select.select(sl,sl,sl)
 
-            for notified_socket in read_socket:
-                print("ready to receive")
-                time.sleep(0.2)
-                data = notified_socket.recv(2)
-                if data :
-                    print(data.decode('utf-8'))
-                if data == b"": # prolly cant to it like this
-                    print("Closed connection")
-                    sys.close()
+            if self.client_socket in read_socket:
+                self.receive(MSG_LEN)
 
-            for notified_socket in write_socket:
-                i+=1
+            if self.client_socket in write_socket:
                 if i % 1000000 == 0:
-                    print(k % 10000)
                     k+=1
-                    message = f"{k}"
-                    len = self.client_socket.send(message.encode('utf-8'))
-                    print(f"Sent: {len}")
+                    message = f"ADMIN:[{k}]"
+                    self.send(message)
+                if k >= 8 :
+                    log.debug("Client ends its work")
+                    self.client_socket.close()
+                    sys.exit()
 
-
-
-            for notified_socket in exception_socket:
-                print("exception occured")
+            if self.client_socket in exception_socket:
+                """what do we hande here?"""
+                log.critical("Error on socket or smth")
+                self.client_socket.close()
                 sys.exit()
-                #again idk
 
 
 
+    def send(self,message):
+        try:
+            total_sent = 0
+            while total_sent < len(message):
+                sent = self.client_socket.send(message[total_sent:].encode('utf-8'))
+                if sent == 0:
+                    log.error("Lost connection")
+
+                log.debug(f"[{sent}] bytes sent: -> {message[total_sent:]} ")
+                total_sent += sent
+
+        except socket.error as e:
+            log.error("Connection broken")
+            log.error(e)
+            self.client_socket.close()
+            sys.exit()
+
+    def receive(self,mess_len):
+
+        try:
+            r_data = self.client_socket.recv(MSG_LEN)
+            # do something with data: append/check etc.
+            log.debug(f"[{len(r_data)}] bytes received : {r_data.decode('utf-8')} ")
+
+            if r_data == b"":
+                """ handling closed connection from server """
+                log.info("Server closed connection ")
+                self.client_socket.close()
+                time.sleep(10)
+                sys.exit()
+
+        except socket.error as e:#ConnectionResetError
+            log.error("Connection broken")
+            log.error(e)
+            self.client_socket.close()
+            sys.exit()
+            #self.client_socket.close()
 
 
+    """ czy napewno potrzbne"""
+    def recoonnect(self):
+        pass
 
 
-
-
-client = Client(HOST,PORT)
-client.start()
+log.debug('Client import succesful.')
