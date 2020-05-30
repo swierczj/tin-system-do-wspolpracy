14bf813b08f15fc094f57c5bb3409b47b994708c
mmakos <misiek.makos@gmail.com>
2020-05-30 15:55:33 +0200

    Create GUI start

--- javaClient/src/client/main.java
+++ javaClient/src/client/main.java
@@ -1,24 +1,32 @@
 package client;
 
-import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class main{
-    public static void main( String args[] ) throws IOException{
+public class main extends Application{
+
+    @Override
+    public void start( Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("generate20.fxml"));
+        primaryStage.setTitle("Generator");
+        primaryStage.setScene(new Scene(root, 640, 480));
+        primaryStage.setResizable( false );
+        primaryStage.show();
 
         String ip = "localhost";
         int port = 54000;
 
-        if( args.length > 0 )
-            ip = args[ 0 ];
-        if( args.length > 1 )
-            port = Integer.parseInt( args[ 1 ] );
-
         Client client = new Client( ip, port );
         if( client.connect() == 0 ){
             client.createThreads();
-
             client.startThreads();
         }
+    }
 
+    public static void main(String[] args) {
+        launch(args);
     }
 }
